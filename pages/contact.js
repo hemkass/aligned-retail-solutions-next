c895c4c68b06700b09aeed6b7a073b316f566f3e
unknown <marinecorbel@yahoo.fr>
2022-03-04 12:39:07 +0100

    response form

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Head from "next/head";
 import styles from "../styles/Contact.module.scss";
+import axios from "axios";
 
 const Contact = () => {
   const [firstname, setFirstname] = useState("");
@@ -8,6 +9,7 @@ const Contact = () => {
   const [email, setEmail] = useState("");
   const [tel, setTel] = useState("");
   const [mess, setMess] = useState("");
+  const [response, setResponse] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -37,50 +39,59 @@ const Contact = () => {
       <div className={styles.contact}>
         <div className={styles.formLeft}>
           <h1>NEED HELP WITH YOUR SUPPLY CHAIN?</h1>{" "}
-        </div>
-        <form onSubmit={handleSubmit}>
-          <div className={styles.dataDiv}>
-            <input
-              onChange={(event) => setFirstname(event.target.value)}
-              type="text"
-              placeholder="Firstname"
-              value={firstname}
-            ></input>{" "}
-            <input
-              onChange={(event) => setLastname(event.target.value)}
-              type="text"
-              placeholder="Lastname"
-              value={lastname}
-            ></input>
+        </div>{" "}
+        {response ? (
+          <div className={styles.response}>
+            <p>Message sent,</p>
+            <p>We will contact you as soon as possible</p>
           </div>
-          <div>
-            <input
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <div className={styles.dataDiv}>
+              <input
+                onChange={(event) => setFirstname(event.target.value)}
+                type="text"
+                placeholder="Firstname"
+                value={firstname}
+              ></input>{" "}
+              <input
+                onChange={(event) => setLastname(event.target.value)}
+                type="text"
+                placeholder="Lastname"
+                value={lastname}
+              ></input>
+            </div>
+            <div>
+              <div className={styles.dataDiv}>
+                <input
+                  required="required"
+                  onChange={(event) => setEmail(event.target.value)}
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                ></input>
+                <input
+                  onChange={(event) => setTel(event.target.value)}
+                  type="text"
+                  placeholder="Your phone"
+                  value={tel}
+                ></input>
+              </div>
+            </div>
+            <textarea
+              className={styles.messageBox}
               required="required"
-              onChange={(event) => setEmail(event.target.value)}
-              type="email"
-              placeholder="Email"
-              value={email}
-            ></input>
-            <input
-              onChange={(event) => setTel(event.target.value)}
+              onChange={(event) => setMess(event.target.value)}
               type="text"
-              placeholder="Your phone"
-              value={tel}
-            ></input>
-          </div>
-          <textarea
-            className={styles.messageBox}
-            required="required"
-            onChange={(event) => setMess(event.target.value)}
-            type="text"
-            placeholder="Your Message"
-            value={mess}
-          ></textarea>
-          <div className={styles.submitButton}>
-            <button>Send message</button>
-          </div>
-          <div className={styles.submitButton}></div>
-        </form>
+              placeholder="Your Message"
+              value={mess}
+            ></textarea>
+            <div className={styles.submitButton}>
+              <button>Send message</button>
+            </div>
+            <div className={styles.submitButton}></div>
+          </form>
+        )}
       </div>
     </>
   );
